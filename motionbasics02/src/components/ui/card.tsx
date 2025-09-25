7926feb3e8e41ab551012c0d6c453b56d0676bca
vedant ghodki <vedantghodki@gmail.com>
2025-09-26 02:56:30 +0530

    Add framer-motion and motion packages; enhance Card component with animations

--- motionbasics02/src/components/ui/card.tsx
+++ motionbasics02/src/components/ui/card.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { cn } from "@/lib/utils"
-import { MessageCircle, MessageSquare, X } from "lucide-react"
+import { ChartNoAxesColumn, ClipboardPenLine, Clock7, MessageCircle, MessageSquare, Plus, X } from "lucide-react"
 import Image from "next/image"
+import { easeInOut, motion } from "motion/react"
 
 export const Card = () => {
     return <div className={cn(
-        "w-72 min-h-[26rem] h-[26rem] bg-white rounded-2xl px-4 py-2 text-black", // Fixed: removed space and changed bg-white-400 to bg-white
+        "w-72 min-h-[26rem] h-[28rem] bg-white rounded-2xl px-4 py-2 text-black", // Fixed: removed space and changed bg-white-400 to bg-white
         "shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)]",
         "flex flex-col items-center"
     )}>
@@ -21,8 +22,26 @@ export const Card = () => {
                 <X className="h-3 w-4 text-neutral-400"/>
             </button>
         </div>
-        <div className="bg-gray-100  flex-1 w-full mt-4 mb-2 rounded-lg border border-dashed border-neutral-200 relative">
-            <div className="absolute inset-0 h-full w-full bg-white rounded-lg divide-y divide-neutral-300">
+        <div
+        className="bg-gray-100 flex-1 w-full mt-4 mb-2 rounded-lg border border-dashed border-neutral-200 relative">
+            <motion.div 
+             initial={{
+                opacity: 20,
+                scale: 0.98,
+                filter: "blur(10px)",
+            }} 
+            whileHover={{
+                opacity: 1,
+                scale: 1,
+                filter: "blur(0px)"
+            }}
+            transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 10,
+                mass: 1,
+            }}
+            className="absolute inset-0 h-full w-full border border-neutral-200 bg-white rounded-lg divide-y divide-neutral-300">
                 <div className="flex gap-2 p-4">
                     <div className="h-7 w-7 flex-shrink-0 bg-gradient-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
                         <MessageSquare size={16} />
@@ -32,7 +51,42 @@ export const Card = () => {
                         <p className="text-neutral-500 text-[8px] mt-0.5">Your everyday component need fulfilled</p>
                     </div>
                 </div>
-            </div>
+                <div className="flex gap-2 p-4">
+                    <div className="h-7 w-7 flex-shrink-0 bg-gradient-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                        <ChartNoAxesColumn size={16} />
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-[9px] font-bold text-neutral-600 mt-0.5">Analytical Approach</p>
+                        <p className="text-neutral-500 text-[8px] mt-0.5">Software monitoring 24/7</p>
+                    </div>
+                </div>
+                <div className="flex gap-2 p-4">
+                    <div className="h-7 w-7 flex-shrink-0 bg-gradient-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                        <ClipboardPenLine size={16} />
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-[9px] font-bold text-neutral-600 mt-0.5">Tech consultation services</p>
+                        <p className="text-neutral-500 text-[8px] mt-0.5">Strategic growth and scalling mechanism using our holistic framework</p>
+                    </div>
+                </div>
+                <div className="flex gap-2 p-4">
+                    <div className="h-7 w-7 flex-shrink-0 bg-gradient-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                        <Clock7 size={16} />
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-[9px] font-bold text-neutral-600 mt-0.5">24/7 Support</p>
+                        <p className="text-neutral-500 text-[8px] mt-0.5">Cloff's team is available 24/7 for your software needs</p>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center gap-2 p-4">
+                    <div className="h-5 w-5 flex-shrink-0 bg-gradient-to-br shadow-[0_1px_1px_rgba(0,0,0,0.05),0_4px_6px_rgba(34,42,53,0.04),0_24px_68px_rgba(47,48,55,0.05),0_2px_3px_rgba(0,0,0,0.04)] bg-white rounded-md flex items-center justify-center">
+                    <Plus size={12} />
+                    </div>
+                    <div className="flex flex-col">
+                        <p className="text-neutral-400 text-[10px]">Get Started</p>
+                    </div>
+                </div>
+            </motion.div>
         </div> {/* Added w-full to make it visible */}
     </div>
 }
